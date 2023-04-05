91f077165c2993124092cd1375b5a7202e7b039a
Anny <98042798+AnnyLai@users.noreply.github.com>
2023-04-05 13:48:32 +0000

    v2

--- .ccls-cache/@home@runner@2hw0203/main.c
+++ .ccls-cache/@home@runner@2hw0203/main.c
@@ -29,6 +29,7 @@ int main()
   }
 
   pOut = fopen( "output.txt" , "w+" );
+  fclose( pOut );
 
   char inputstr[2001] = {0};
   fread( &inputstr[0] , 1 , 2000 , pFile );
@@ -41,8 +42,8 @@ int main()
   char key[256] = {0};
   key[0] = ' ';
   printf("Please enter the search target: ");
-  fgets( &key[1] , 256 , stdin );
-  for( size_t i = 0 ; i < 256 ; i++ )
+  fgets( &key[1] , 255 , stdin );
+  for( size_t i = 1 ; i < 256 ; i++ )
   {
     if( key[i] == 0 )
     {
@@ -54,32 +55,21 @@ int main()
     }
   }
 
-  bibletext *bible = NULL;
-  bibletext *now = NULL;
   int32_t i = 0;
   int32_t times = 0;
   bibletext *tempt = (bibletext *)calloc( 1 , sizeof(bibletext) );
-
+  
   while( !feof(pFile) )
-  //for( int l = 0 ; l < 1495 ; l++ )
   {
     if( i == 2000 )
     {
       fread( inputstr , 1 , 2000 , pFile );
-      //puts( &(inputstr[0]) );
       i = 0;
     }
     bible_init( tempt );
     char tag[15] = {0};
     int32_t j = 1;
-    int32_t q = 0;
 
-    //printf( "bookname[0] = %c\n" , tempt->book_name[0] );
-
-    //printf( "b\n" );
-    //printf( "%c" , inputstr[i] );
-    //puts( &inputstr[0] );
-    //printf( "%c\n" , inputstr[0] );
     if( inputstr[i] == '{' )
     {
       while( inputstr[i] != '}' )
@@ -90,7 +80,6 @@ int main()
           if( i+j == 2000 )
           {
             fread( inputstr , 1 , 2000 , pFile );
-            //puts( &inputstr[0] );
             i = -j;
           }
           for( size_t s = 0 ; s < 15 ; s++ )
@@ -106,7 +95,6 @@ int main()
             if( i+j == 2000 )
             {
               fread( inputstr , 1 , 2000 , pFile );
-              //puts( &inputstr[0] );
               i = -j;
             }
           }
@@ -321,24 +309,15 @@ int main()
         if( i == 2000 )
         {
           fread( inputstr , 1 , 2000 , pFile );
-          //puts( &inputstr[0] );
           i = 0;
         }
       }
 
-      /*int o = 0;
-      while( tempt -> book_id[o] != 0 )
-      {
-        putchar( tempt -> book_id[o] );
-        o++;
-      }
-      printf( " %d:%d " , tempt->chapter , tempt->verse );
-      puts( &(tempt -> text[0]) );*/
-
       if( strstr( &(tempt->text[0]) , &key[0]) != NULL )
       {
+        pOut = fopen( "output.txt" , "a+" );
         times++;
-        char *num = calloc( 5 , sizeof(char) );
+	char *num = calloc( 7 , sizeof(char) );
         int32_t Q = 0;
         if( times/10000 != 0 )
         {
@@ -351,6 +330,8 @@ int main()
           num[3] = Q / 10 + '0';
           Q = times % 10;
           num[4] = Q + '0';
+          num[5] = '.';
+          num[6] = ' ';
         }
         else if( times/1000 != 0 )
         {
@@ -361,6 +342,9 @@ int main()
           num[2] = Q / 10 + '0';
           Q = times % 10;
           num[3] = Q + '0';
+          num[4] = '.';
+          num[5] = ' ';
+	  num[6] = 0;
         }
         else if( times/100 != 0 )
         {
@@ -369,19 +353,33 @@ int main()
           num[1] = Q / 10 + '0';
           Q = times % 10;
           num[2] = Q + '0';
+          num[3] = '.';
+          num[4] = ' ';
+	  num[5] = 0;
+	  num[6] = 0;
         }
         else if( times/10 != 0 )
         {
           num[0] = (times / 10) + '0';
           Q = times % 10;
           num[1] = Q + '0';
+          num[2] = '.';
+          num[3] = ' ';
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
         else
         {
           num[0] = times + '0';
+          num[1] = '.';
+          num[2] = ' ';
+	  num[3] = 0;
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
-        int32_t size = strlen(&num[0]);
-        fwrite( &num[0] , 1 , size , pOut );
+        fwrite( num , 1 , strlen(num) , pOut );
         fwrite( &(tempt->book_id[0]) , 1 , strlen(&(tempt->book_id[0])) , pOut );
         Q = 0;
         num[0] = ' ';
@@ -393,6 +391,8 @@ int main()
           Q = tempt->chapter % 10;
           num[3] = Q + '0';
           num[4] = ':';
+	  num[5] = 0;
+	  num[6] = 0;
         }
         else if( tempt->chapter/10 != 0 )
         {
@@ -400,13 +400,20 @@ int main()
           Q = tempt->chapter % 10;
           num[2] = Q + '0';
           num[3] = ':';
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
         else
         {
           num[1] = tempt->chapter + '0';
           num[2] = ':';
+	  num[3] = 0;
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
-        fwrite( &num[0] , 1 , strlen(&num[0]) , pOut );
+        fwrite( num , 1 , strlen(num) , pOut );
         Q = 0;
         if( tempt->verse/100 != 0 )
         {
@@ -416,6 +423,9 @@ int main()
           Q = tempt->verse % 10;
           num[2] = Q + '0';
           num[3] = ' ';
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
         else if( tempt->verse/10 != 0 )
         {
@@ -423,53 +433,25 @@ int main()
           Q = tempt->verse % 10;
           num[1] = Q + '0';
           num[2] = ' ';
+	  num[3] = 0;
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
         else
         {
           num[0] = tempt->verse + '0';
           num[1] = ' ';
+	  num[2] = 0;
+	  num[3] = 0;
+	  num[4] = 0;
+	  num[5] = 0;
+	  num[6] = 0;
         }
-        fwrite( &num[0] , 1 , strlen(&num[0]) , pOut );
+        fwrite( num , 1 , strlen(num) , pOut );
         fprintf( pOut , "%s\n" , &(tempt -> text[0]) );
-        /*if( !now )
-        {
-          bible = (bibletext *)calloc( 1 , sizeof(bibletext) );
-          bible_init( bible );
-          now = bible;
-        }
-        else
-        {
-          now -> next = (bibletext *)calloc( 1 , sizeof(bibletext) );
-          bible_init( now->next );
-          now = now -> next;
-        }
-        
-        now -> chapter = tempt -> chapter;
-        now -> verse = tempt -> verse;
-        q = 0;
-        while( tempt -> text[q] != 0 )
-        {
-          now -> text[q]  = tempt -> text[q];
-          q++;
-        }
-        q = 0;
-        while( tempt -> translation_id[q] != 0 )
-        {
-          now -> translation_id[q] = tempt -> translation_id[q];
-          q++;
-        }
-        q = 0;
-        while( tempt -> book_id[q] != 0 )
-        {
-          now -> book_id[q] = tempt -> book_id[q];
-          q++;
-        }
-        q = 0;
-        while( tempt -> book_name[q] != 0 )
-        {
-          now -> book_name[q] = tempt -> book_name[q];
-          q++;
-        }*/
+        free( num );
+	fclose( pOut );
       }
     }
 
@@ -477,38 +459,27 @@ int main()
     if( i == 2000 )
     {
       fread( inputstr , 1 , 2000 , pFile );
-      puts( &inputstr[0] );
       i = 0;
     }
   }
 
+  free( tempt );
+  pOut = fopen( "output.txt" , "a" );
+  char end = {0};
+  fwrite( &end , 1 , 1 , pOut );
   fclose( pOut );
+
   pOut = fopen( "output.txt" , "r" );
 
   if( pOut )
   {
     while( !feof(pOut) )
     {
-      fread( &(inputstr[0]) , 1 , 2000 , pOut );
-      inputstr[2000] = 0;
-      puts( &(inputstr[0]) );
+	    fread( &(inputstr[0]) , 1 , 2000 , pOut );
+	    inputstr[2000] = 0;
+	    puts( &(inputstr[0]) );
     }
   }
-  /*now = bible;
-  printf( "Found %d time(s)\n" , times );
-  for( size_t s = 0 ; s < times ; s++ )
-  {
-    printf( "%lu. " , s+1 );
-    i = 0;
-    while( now -> book_id[i] != 0 )
-    {
-      putchar( now -> book_id[i] );
-      i++;
-    }
-    printf( " %d:%d " , now->chapter , now->verse );
-    puts( &(now -> text[0]) );
-    now = now -> next;
-  }*/
 
   fclose( pFile );
   fclose( pOut );
