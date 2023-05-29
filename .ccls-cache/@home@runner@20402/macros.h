db706a6c92cd90a143b33119c66da38a4deda7c8
AnnyLai <anny920504@gmail.com>
2023-05-29 03:19:34 +0000

    v1

---
+++ .ccls-cache/@home@runner@20402/macros.h
@@ -0,0 +1,18 @@
+#include <math.h>
+
+#define BIT(x) ( pow(2 , x) )
+#define SETBIT(x,p) ( x | (1 << p) )
+#define CLEARBIT(x,p) ( x & ~(1 << p) )
+#define GETBIT(x,p) ( (x >> p) % 2 )
+#define TOGGLEBIT(x,p) ( x ^ (1 << p) )
+/* Loop related Macros */
+// Example: RANGE (i, 10, 20)
+// it will be 10,11,12,13,14,15,16,17,18,19,20
+// Example: RANGE (i, 5, -5)
+// it will be 5,4,3,2,1,0,-1,-2,-3,-4,-5
+#define RANGE(i,y,x) calloc( y , x )
+(where did they
+// FOREACH (item, array)
+// A C implementation of foreach loop, it will go through each element of an array , and will perform operations on each element as returned into pointer item variable.
+#define FOREACH(i,A)
+
--- .ccls-cache/@home@runner@20402/main.c
+++ .ccls-cache/@home@runner@20402/main.c
@@ -10,19 +10,7 @@ int main()
   //printf( "Set 10010(18)'s 0 bit as 0 => %d\n" , CLEARBIT(18,0) );
   //printf( "Get 10010(18)'s 4 bit => %d\n" , GETBIT(18,4) );
   //printf( "Turn 10010(18)'s 0 bit as the opposite => %d\n" , TOGGLEBIT(18,0) );
-  /*int32_t i = 0;
-  RANGE( i , 0 , -1 )
-  {
-    printf( "%d " , i );
-  }
-  printf( "\n" );*/
-  int8_t item = 76;
-  int8_t arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  FOREACH( item , arr )
-  for( size_t i = 0 ; i < 10 ; i++ )
-  {
-    printf( "%d\n" , arr[i] );
-  }
+  printf( "Turn 10010(18)'s 0 bit as the opposite => %d\n" , TOGGLEBIT(18,0) );
 
   return 0; 
 }
